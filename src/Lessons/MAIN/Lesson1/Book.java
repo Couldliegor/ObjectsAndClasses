c03e7ff7e1da8f88ef8eb4590459742e2dcc8a03
egorcouldlie <couldliegor@gmail.com>
2022-12-20 18:28:42 +0300

    lesson3

--- src/Lessons/MAIN/Lesson1/Book.java
+++ src/Lessons/MAIN/Lesson1/Book.java
@@ -3,4 +3,10 @@ public class Book {
     public String author;
     public String name;
     public int year;
+
+    public Book(String author, String name, int year) {
+        this.author = author;
+        this.name = name;
+        this.year = year;
+    }
 }
--- src/Lessons/MAIN/Lesson2/Author.java
+++ src/Lessons/MAIN/Lesson2/Author.java
@@ -4,4 +4,8 @@ public class Author {
     public String name;
     public String surname;
 
+    public Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
 }
